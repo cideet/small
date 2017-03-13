50ebb86b82155724ca507e9d972c0759c783ad53
张三丰 <123456@qq.com>
2017-03-13 17:46:23 +0800

    节操碎了一地

--- es6/es5.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-[1, 2, 3].map(function (x) {
-    return x * x;
-});
-
-for (var i = 0; i < 10; i++) {
-    document.write(i);
-}
--- es6/es6.js
+++ es6/es6.js
@@ -3,5 +3,5 @@
 });
 
 for (let i = 0; i < 10; i++) {
-    document.write(i);
+    document.write(i + '<br>');
 }
