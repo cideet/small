e9fb8ccd63a6b12f1172b54fc8d043506c9a50f4
张三丰 <123456@qq.com>
2017-03-17 21:28:31 +0800

    页面的横向布局

--- qixi/me/js/qixi.js
+++ qixi/me/js/qixi.js
@@ -2,4 +2,22 @@
  * Created by sf on 2017/3/17.
  */
 
+var container = $('#content');
+var element = container.find(':first');
+var slides = element.find('>');
+var width = container.width();
+var height = container.height();
+console.log(width + "" + height);
 
+element.css({
+    width: width * slides.length + 'px',
+    height: height + 'px'
+});
+
+$.each(slides, function (index) {
+    var slide = slides.eq(index);
+    slide.css({
+        width: width + 'px',
+        height: height + 'px'
+    });
+});
